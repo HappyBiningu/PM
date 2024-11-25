d3005a2b70d3d92fd0c8c7abcef8cf7f18b02f30
Tinotenda Biningu <happybiningu@icloud.com>
2024-11-25 12:47:00 +0200

    changed login,register file

--- users/forms.py
+++ users/forms.py
@@ -7,10 +7,35 @@ class UserRegistrationForm(forms.ModelForm):
     surname = forms.CharField(max_length=50, required=True)
     email = forms.EmailField(required=True)
     role = forms.ChoiceField(choices=Profile.ROLE_CHOICES, required=True)
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': "Enter your password"}),
+        required=True
+    )
+    password_confirmation = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': "Confirm your password"}),
+        required=True
+    )
 
     class Meta:
         model = User
-        fields = ['username', 'password']
+        fields = ['username', 'password', 'email']
+     
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.help_text = None  
+        self.fields['username'].label = "Enter Username"
+        self.fields['email'].widget.attrs['placeholder'] = "Enter your email address"
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_confirmation = cleaned_data.get("password_confirmation")
+
+        if password != password_confirmation:
+            raise forms.ValidationError("Passwords do not match.")
+        return cleaned_data
 
     def save(self, commit=True):
         user = super().save(commit=False)
